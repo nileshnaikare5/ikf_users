20b193b5d76f3bae2ee88038eb0d3d40dea7c974
nileshnaikare5 <72270145+nileshnaikare5@users.noreply.github.com>
2022-02-14 02:22:26 +0530

    Release 1.0.1

---
+++ Userwebapp/Userwebapp/App_Code/DAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Userwebapp
+{
+    public class DAL
+    {
+        DbClass dbClass = new DbClass();
+        SqlCommand sqlcmd;
+        public DataTable GetUsers()
+        {
+            DataTable dtUsers = new DataTable();
+            sqlcmd = new SqlCommand();
+            dtUsers = dbClass.returnDataTable("usp_GetUsers", sqlcmd);
+            return dtUsers;
+
+        }
+
+        public int AUD_User(int UserID,string Name, string DOB, string Designation, string Skills, string AUFlag)
+        {
+            int iUserID = 0;
+            try
+            {
+                sqlcmd = new SqlCommand();
+                sqlcmd.Parameters.Add("@UserId", SqlDbType.Int).Value = UserID;
+                sqlcmd.Parameters.Add("@Name", SqlDbType.VarChar).Value = Name;
+                sqlcmd.Parameters.Add("@DOB", SqlDbType.VarChar).Value = DOB;
+                sqlcmd.Parameters.Add("@Designation", SqlDbType.VarChar).Value = Designation;
+                sqlcmd.Parameters.Add("@Skills", SqlDbType.VarChar).Value = Skills;
+                sqlcmd.Parameters.Add("@AuFlag", SqlDbType.Char).Value = AUFlag;
+                iUserID = dbClass.returnInteger("usp_Users", sqlcmd);
+            }
+            catch (Exception ex) { }
+            return iUserID;
+        }
+    }
+}
---
+++ Userwebapp/Userwebapp/App_Code/DbClass.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Userwebapp
+{
+    public class DbClass
+    {
+        private SqlConnection connect;
+        private SqlCommand cmd;
+        private SqlDataAdapter datadptr;
+        public DataSet ds;
+        
+
+        // Function to get connection.
+        public void DatabaseConnection()
+        {
+                       
+            connect = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ToString());
+        }
+
+        // Function to open connection to DB.
+        public SqlConnection Openconn()
+        {
+            DatabaseConnection();
+            if (connect.State == ConnectionState.Closed || connect.State == ConnectionState.Broken)
+            {
+                connect.Open();
+            }
+            return connect;
+        }
+
+        // Function to close connection to DB.
+        public SqlConnection Closeconn()
+        {
+            if (connect.State == ConnectionState.Open)
+            {
+                connect.Close();
+            }
+            return connect;
+        }
+
+        // Function to Insert,Update & Deleting values in DB.
+        public void ExecuteQuery(string SProcName, SqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = Openconn();
+                cmd.CommandText = SProcName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                Closeconn();
+            }
+
+        }
+
+        // Function to return values using Dataset.
+        public DataSet returnDataSet(string SProcName, SqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = Openconn();
+                cmd.CommandText = SProcName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                datadptr = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+                datadptr.Fill(ds);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Closeconn();
+            }
+            return (ds);
+        }
+
+        // Function to return values using Data-Table.
+        public DataTable returnDataTable(string SProcName, SqlCommand cmd)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                cmd.Connection = Openconn();
+                cmd.CommandText = SProcName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                datadptr = new SqlDataAdapter(cmd);
+                datadptr.Fill(dt);
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                Closeconn();
+            }
+            return dt;
+        }
+
+        // Function to return  string values using Execute Scalar.
+        public string returnString(string SProcName, SqlCommand cmd)
+        {
+            string returnValue = "";
+            try
+            {
+                cmd.Connection = Openconn();
+                cmd.CommandText = SProcName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                returnValue = (string)cmd.ExecuteScalar();
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                Closeconn();
+            }
+            return (returnValue);
+        }
+
+        // Function to return  integer values using Execute Scalar.
+        public int returnInteger(string SProcName, SqlCommand cmd)
+        {
+            int returnValue = 0;
+            try
+            {
+                cmd.Connection = Openconn();
+                cmd.CommandText = SProcName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                returnValue = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                Closeconn();
+            }
+            return (returnValue);
+        }
+    }
+}
